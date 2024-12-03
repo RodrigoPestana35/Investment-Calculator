d096d6ea22574fdf6b290331db0529b970edeb70
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-12-03 22:41:50 +0000

    update initial investment data values and ensure proper float conversion in input handling

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,16 @@ import ResultsTable from "./components/ResultsTable"
 
 function App() {
   let [investmentData, setInvestmentData] = useState({   
-    initialInvestment: 0,
-    annualInvestment: 0,
-    expectedReturn: 0,
-    duration: 0,
+    initialInvestment: 15000,
+    annualInvestment: 900,
+    expectedReturn: 6,
+    duration: 10,
   })
 
   function handleInvestmentDataChange(event, name) {
     /*let name = event.target.name  podia fazer desta forma, mas a logica não esta correta
     name = name.toLowerCase().replace(" ", "")*/
-    let value = event.target.value
+    let value = parseFloat(event.target.value)  //converte o valor para float
     setInvestmentData(prevData => {  //atualiza o estado do objeto investmentData
       if(!value){   //se o valor for vazio, o valor é 0
         value = 0
