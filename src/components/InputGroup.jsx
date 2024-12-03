3022ea6b31d18d9a18f4f12842388e68fac7372c
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-12-03 21:12:38 +0000

    refactor InputGroup component layout for improved structure and styling

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -1,14 +1,26 @@
 export default function InputGroup() {
     return (
         <div id="user-input">
-            <label> INITIAL INVESTMENT</label>
-            <input type="number" />
-            <label>ANUAL INVESTMENT</label>
-            <input type="number" />
-            <label>EXPECTED RETURN</label>
-            <input type="number" />
-            <label>DURATION {"(IN YEARS)"}</label>
-            <input type="number" />
+            <div className="input-group">
+                <div>
+                    <label> INITIAL INVESTMENT</label>
+                    <input type="number" />
+                </div>
+                <div>
+                    <label>ANUAL INVESTMENT</label>
+                    <input type="number" />
+                </div>
+            </div>
+            <div className="input-group">
+                <div>
+                    <label>EXPECTED RETURN</label>
+                    <input type="number" />
+                </div>
+                <div>
+                    <label>DURATION {"(IN YEARS)"}</label>
+                    <input type="number" />
+                </div>
+            </div>
         </div>
     )
 }
