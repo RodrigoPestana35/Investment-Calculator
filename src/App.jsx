a78fb5e39b55e34622dab00cc167368be9770067
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-12-03 21:07:50 +0000

    add InputGroup component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header"
+import InputGroup from "./components/InputGroup"
 
 function App() {
   return (
     <>
       <Header />
+      <InputGroup />
     </>
   )
 }
---
+++ src/components/InputGroup.jsx
@@ -0,0 +1,14 @@
+export default function InputGroup() {
+    return (
+        <div id="user-input">
+            <label> INITIAL INVESTMENT</label>
+            <input type="number" />
+            <label>ANUAL INVESTMENT</label>
+            <input type="number" />
+            <label>EXPECTED RETURN</label>
+            <input type="number" />
+            <label>DURATION {"(IN YEARS)"}</label>
+            <input type="number" />
+        </div>
+    )
+}
