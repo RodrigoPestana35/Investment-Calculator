ed8cd04bc5338b33b730482078b281365d1a71c7
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-12-06 12:58:48 +0000

    refactor InputGroup component to use semantic HTML and improve structure for styling

--- src/components/InputGroup.jsx
+++ src/components/InputGroup.jsx
@@ -1,16 +1,24 @@
 import Input from "./Input";
 
-export default function InputGroup({investmentDataHandler, data}) {
+export default function InputGroup({ investmentDataHandler, data }) {
     return (
-        <div id="user-input">
+        <section id="user-input">
             <div className="input-group">
-                <Input labelName="INITIAL INVESTMENT" investmentDataHandler={(e) => investmentDataHandler(e, "initialInvestment")} value={data.initialInvestment}/>
-                <Input labelName="ANNUAL INVESTMENT" investmentDataHandler={(e) => investmentDataHandler(e, "annualInvestment")} value={data.annualInvestment}/>
+                <p>
+                    <Input labelName="INITIAL INVESTMENT" investmentDataHandler={(e) => investmentDataHandler(e, "initialInvestment")} value={data.initialInvestment} />
+                </p>
+                <p>
+                    <Input labelName="ANNUAL INVESTMENT" investmentDataHandler={(e) => investmentDataHandler(e, "annualInvestment")} value={data.annualInvestment} />
+                </p>
             </div>
             <div className="input-group">
-                <Input labelName="EXPECTED RETURN" investmentDataHandler={(e) => investmentDataHandler(e, "expectedReturn")} value={data.expectedReturn}/>
-                <Input labelName="DURATION" investmentDataHandler={(e) => investmentDataHandler(e, "duration")} value={data.duration}/>
+                <p>
+                    <Input labelName="EXPECTED RETURN" investmentDataHandler={(e) => investmentDataHandler(e, "expectedReturn")} value={data.expectedReturn} />
+                </p>
+                <p>
+                    <Input labelName="DURATION" investmentDataHandler={(e) => investmentDataHandler(e, "duration")} value={data.duration} />
+                </p>
             </div>
-        </div>
+        </section>
     )
 }
